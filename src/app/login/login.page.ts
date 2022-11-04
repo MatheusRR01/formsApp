75f8da6ee3d8f2b219699f4c7fe2a26f7d348bcc
Matheus Rovero Rodrigues <vector.320@outlook.com>
2022-11-04 20:38:50 -0300

    Verificações nos campos de e-mail e senha na pagina login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+  });
+
+  mensagensErro = {
+    email: [{tipo: 'required', aviso:'Campo obrigatório!'}, {tipo:'email', aviso: 'Esse campo deve ser um e-mail válido!'}],
+    senha: [{tipo: 'required', aviso:'Campo obrigatório!'}, {tipo:'minLength', aviso: 'Deve possuir no mínimo 6 caracteres!'}],
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
